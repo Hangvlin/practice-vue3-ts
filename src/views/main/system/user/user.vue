3ad78d6a6701fee1780a810389c4386ef3e6c508
codeMakerLin <961777537@qq.com>
2021-08-08 20:48:59 +0800

    feat(all): 尝试封装高级组件

--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>123</div>
+  <div class="user">
+    <hy-form v-bind="searchFormConfig" />
+    <div class="content"></div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import HyForm from '@/base-ui/form'
+
+import { searchFormConfig } from './config/search.config'
 
 export default defineComponent({
+  name: 'user',
+  components: {
+    HyForm
+  },
   setup() {
-    return {}
+    return {
+      searchFormConfig
+    }
   }
 })
 </script>
